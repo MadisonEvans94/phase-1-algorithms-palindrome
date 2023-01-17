2462b9172f07a065d9df61483f01e65eaab4a2b4
Madison Evans <m.corbinevans@gmail.com>
2023-01-17 11:12:29 -0700

    commit tree c582b01adc3c56bbd980584cb8aab67232a8899f

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+	// Write your algorithm here
+	let pointer1 = 0;
+	let pointer2 = word.length - 1;
+	while (pointer1 < pointer2) {
+		if (word[pointer1].toLowerCase() !== word[pointer2].toLowerCase()) {
+			return false;
+		} else {
+			pointer1++;
+			pointer2--;
+		}
+	}
+	return true;
 }
 
 /* 
@@ -12,14 +23,14 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+	// add your own custom tests in here
+	console.log("Expecting: true");
+	console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+	console.log("Expecting: false");
+	console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
